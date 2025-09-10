c44811931f538a32601d2e83955f0157497364aa
CemalSezer <infocemalsezer@gmail.com>
2025-09-10 15:59:19 +0300

    deploy operations are added

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://web-dev-app-xi.vercel.app/',
+});
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,14 +1,29 @@
 import { User } from '../types/user';
 import { Post } from '../types/post';
 
+// export const fetchUsers = async (): Promise<User[]> => {
+//   const res = await fetch('http://localhost:3000/users');
+//   if (!res.ok) throw new Error('Failed to fetch users');
+//   return res.json();
+// };
+
+// export const fetchPosts = async (): Promise<Post[]> => {
+// const res = await fetch('http://localhost:3000/posts');
+//   if (!res.ok) throw new Error('Failed to fetch posts');
+//   return res.json();
+// };
+
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 export const fetchUsers = async (): Promise<User[]> => {
-  const res = await fetch('http://localhost:3000/users');
+  const res = await fetch(`${BASE_URL}/users`);
   if (!res.ok) throw new Error('Failed to fetch users');
   return res.json();
 };
 
 export const fetchPosts = async (): Promise<Post[]> => {
-const res = await fetch('http://localhost:3000/posts');
+  const res = await fetch(`${BASE_URL}/posts`);
   if (!res.ok) throw new Error('Failed to fetch posts');
   return res.json();
 };
